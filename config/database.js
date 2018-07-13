3154f8c100584b5363ea8c6c32c3126571f2db5c
KeithTheDev <kgoodrich44@gmail.com>
2018-07-13 10:07:10 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://demo:demodemo1@ds127851.mlab.com:27851/savage' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
